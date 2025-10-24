6aee8d73ad8165b8b1fb8a2ff2aa313c2b5d33db
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-24 16:00:07 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
 
+export default function App(){
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Navbar />
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/products" element={<Products/>} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
