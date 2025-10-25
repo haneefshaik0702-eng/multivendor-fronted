76ef51446906acd839f8ec31b6d24da2dbe831de
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-25 10:03:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist', // 👈 Vite will output files into the "dist" folder
+  },
 })
